ace0e285f9106f9e3bab3825f9d06e5273fab4d5
Surya Sekhar Sharma <sekharsurya369@gmail.com>
2025-02-14 18:43:00 +0530

    update Next.js configuration to enable TypeScript error ignoring, unoptimized images, and ESLint build ignoring

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,18 @@
 // import {withSentryConfig} from '@sentry/nextjs';
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    typescript: {
+        ignoreBuildErrors: true,
+    },
+    images: {
+        unoptimized: true,
+    },
+    eslint: {
+        ignoreDuringBuilds: true,
+    },
+    webpack: (config) => {
+        config.resolve.fallback = { fs: false, net: false, tls: false };
+        return config;
+    },
+};
 
